eb7a0fe9d523912a66ae91f6b5674a8eb04a63c8
MH Rose <mhasanrose888@gmail.com>
2023-08-12 23:16:36 +0600

    rest country task comleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  // const [addCountry, setAddCountry] = useState([]); 
+  // alternative
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v2/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    .then(error => console.log(error))
+  }, [])
+  const handleAddCountry = (country) => {
+    // const newAddCountry = [...addCountry, country]
+    // setAddCountry(newAddCountry)
+    const newCart = [...cart, country]
+    setCart(newCart)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <h1>Country Loaded: {countries.length}</h1>
+    <h4>Country Added: {cart.length}</h4>
+    <Cart cart={cart}></Cart>
+      {
+        countries.map(country => <Country country= {country} handleAddCountry={handleAddCountry} key= {country.alpha3Code}></Country>)
+      }
+      
+   </div>
   );
 }
 
